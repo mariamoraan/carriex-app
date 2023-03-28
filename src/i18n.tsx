0ffcd9adb802add4890fdaada49825509a4cd3bf
María Morán <maria.moran@getorca.com>
2023-03-28 15:53:23 +0200

    feat. language detector reactivated

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,4 +1,5 @@
 import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import localizableEN from './localizables/en/localizableEN.json';
 import localizableES from './localizables/es/localizableES.json';
@@ -13,7 +14,7 @@ const resources = {
 }
 
 i18next
-//.use(LanguageDetector)
+.use(LanguageDetector)
 .use(initReactI18next)
 .init({
   resources,
